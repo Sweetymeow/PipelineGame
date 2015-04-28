1b968165bdc6f3e43ea45d0f0fa3a6dc928496b2
Wendy <honeykatie@qq.com>
2015-04-27 23:00:21 -0400

    test parse

--- parse.php
+++ parse.php
@@ -1,20 +1,4 @@
 <?php
-session_start();
-// start a session for each user(end when your close brower).
-$flashKey = isset($_GET['key1']);
-
-if ($flashKey) { 
-	// Get Data from Flash
-	$saveKey = $_GET['key1'];
-	$_SESSION["pipeNum"] = $_GET['key1'];
-	$myfile = fopen("pipename.txt", "w") or die("Unable to open file!");
-	fwrite($myfile, $saveKey);
-	$myfile = fopen("pipename.txt", "r") or die("Unable to open file!");
-	echo "Accept key1 data: ";
-	echo fgets($myfile);
-	echo "\n";
-	fclose($myfile);
-}
 // define location of Parse PHP SDK, e.g. location in "Parse" folder
 // Defaults to ./Parse/ folder. Add trailing slash
 define( 'PARSE_SDK_DIR', 'vendor/parse/' );
@@ -32,11 +16,14 @@ ParseClient::initialize('BbrZSp4nxzHdR7KluZ4nhfbwcjnPq0I6mFWd1lSV',
 
 // save something to class TestObject
 $testObject = ParseObject::create("TestObject");
-$testObject->set("UserID", "sweetymeow");
+$testObject->set("UserID", "sweetymeow4");
 $testObject->save();
+echo 'OK2';
 
 // // get the object ID
-// echo $testObject->getObjectId();
+echo $testObject->getObjectId();
+echo 'OK';
+echo '<h1>Users</h1>';
 
 // echo '<h1>Users</h1>';
 
@@ -49,15 +36,4 @@ $testObject->save();
 //   // echo user Usernames
 //   echo $result->get('username') . '<br/>';
 // }
-
-// Get Data from Unity
-if (isset($_GET['getkey'])) {
-	// Read txt file in Sties Folder(LocalHost)
-	$myunityfile= fopen("pipename.txt", "r") or die("Unable to open file!");
-	//echo fgets($myunityfile);
-	fclose($myunityfile);
-	echo $_SESSION["pipeNum"];
-	# code...
-}
-
 ?>
